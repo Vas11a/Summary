4362d48a2aa5035c5b594306082169289a1f1f1a
Vas11a <104201639+Vas11a@users.noreply.github.com>
2022-04-23 12:30:58 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,14 @@
 
 const navigation = document.querySelector('.nav');
 const menuLogo = document.querySelector('.menu_logo');
+const nav = document.querySelector('.nav');
+
+function closeMenu (e) {
+	navigation.classList.remove('visible')
+	menuLogo.classList.remove('logo__menue_cross')
+}
+
+nav.onclick = closeMenu;
 
 function openMenu (e) {
 	if (navigation.classList.contains('visible')) {
